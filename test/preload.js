98104f51577f2b1576ba07e882cc18f383ba3706
Шеогорат <95223648+MSIborisyeltsin@users.noreply.github.com>
2024-05-14 22:19:01 +0700

    Delete test/preload.js

--- test/preload.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var preloader = document.getElementsByClassName("preloader");
-    console.log("[preload.js]: Step 1");
-    
-    preloader.addEventListener("animationend", function(){
-        preloader.remove();
-        console.log("[preload.js]: Step 2");
-    });
-});
